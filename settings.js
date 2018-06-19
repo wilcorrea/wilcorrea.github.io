55a8be111d3d3966436fce50e970a6b1afeb589e
William Correa <wilcorrea@gmail.com>
2018-06-19 09:12:18 -0300

    Update and rename social.js to settings.js

---
+++ settings.js
@@ -0,0 +1,43 @@
+const settings = {
+  avatar: {
+    'background-image': 'url(https://www.gravatar.com/avatar/3be9dea8668cb169e52f024a7a9cffb8?s=200)'
+  },
+  locale: '',
+  i18n: {
+    'en_US': {
+      header: 'Development and Management of Software Projects',
+      bio: 'I work with software development since 2007. I am a bachelor in Computer Science and I try to stay in line with the market. I currently work as Software Engineer at Fagoc (@fagoc.br) and am co-founder and Analyst at Lyseon Tech (@lyseon.tech). I try to move the PHP Group from Zona da Mata where I publish web development materials on the blog and in the community channel. I am always in contact with groups and communities of VueJS, PHP and other technologies throughout Brazil, taking the word of Engineering and Software Architecture in search of producing quality applications.'
+    },
+    'pt_BR': {
+      header: 'Desenvolvimento e Gestão de Projetos de Software',
+      bio: 'Trabalho com desenvolvimento de softwares desde 2007. Sou bacharel em Ciência da Computação e procuro manter-me alinhado ao mercado. Atualmente atuo como Engenheiro de Software na Fagoc (@fagoc.br) e sou co-fundador e Analista na Lyseon Tech (@lyseon.tech). Procuro movimentar o Grupo de PHP da Zona da Mata onde publico materiais sobre desenvolvimento web no blog e no canal da comunidade. Estou sempre em contato com grupos e comunidades de VueJS, PHP e outras tecnologias pelo Brasil afora levando a palavra da Engenharia e Arquitetura de Software em busca de produzirmos aplicações de qualidade.'
+    }
+  },
+  social: [
+    {
+      href: 'https://twitter.com/wilcorrea',
+      class: 'fa fa-twitter'
+    },
+    {
+      href: 'https://facebook.com/wilcorrea.rocks',
+      class: 'fa fa-facebook-official'
+    },
+    {
+      href: 'https://github.com/wilcorrea',
+      class: 'fa fa-github',
+    },
+    {
+      href: 'https://instagram.com/wilcorrea',
+      class: 'fa fa-instagram'
+    },
+    {
+      href: 'https://linkedin.com/in/wilcorrea',
+      class: 'fa fa-linkedin'
+    },
+    {
+      href: 'mailto:me@wilcorrea.rocks',
+      class: 'fa fa-envelope-o'
+    }
+  ]
+}
+

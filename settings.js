5be9a8a1586b83721f03162f1d854e3298003095
William C�rrea <wilcorrea@gmail.com>
2019-05-09 19:05:05 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,11 +6,11 @@ const settings = {
   i18n: {
     'en_US': {
       header: 'Development and Management of Software Projects',
-      bio: 'I work with software development since 2007. I have a bachelor\'s degree in Computer Science and I try to keep in line with the market. I am always in contact with groups and communities of VueJS, PHP and other technologies throughout Brazil. As an evangelist in good practice, engineering and software architecture I am always looking to build quality applications and systems. I write articles and do lectures with git themes and devops for Javascript and VueJS.'
+      bio: 'I work with software development since 2007. I have a bachelor\'s degree in Computer Science and I try to keep in line with the market. I am always in contact with groups and communities of VueJS, PHP and other technologies throughout Brazil. As an evangelist in good practice, engineering and software architecture I am always looking to build quality applications and systems. I write articles and do talks with themes from git and devops to Javascript and VueJS.'
     },
     'pt_BR': {
       header: 'Desenvolvimento e Gestão de Projetos de Software',
-      bio: 'Trabalho com desenvolvimento de software desde 2007. Sou bacharel em Ciência da Computação e tento me manter alinhado com o mercado. Estou sempre em contato com grupos e comunidades de VueJS, PHP e outras tecnologias em todo o Brasil. Como evangelista em boas práticas, engenharia e arquitetura de software estou sempre em busca construir aplicações e sistemas de qualidade. Eu escrevo artigos e faço palestras com temas do git e devops para Javascript e VueJS.'
+      bio: 'Trabalho com desenvolvimento de software desde 2007. Sou bacharel em Ciência da Computação e tento me manter alinhado com o mercado. Estou sempre em contato com grupos e comunidades de VueJS, PHP e outras tecnologias em todo o Brasil. Como evangelista em boas práticas, engenharia e arquitetura de software estou sempre em busca construir aplicações e sistemas de qualidade. Eu escrevo artigos e faço palestras com temas desde git e devops até Javascript e VueJS.'
     }
   },
   social: [

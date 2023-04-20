5e0e7efe3a4f6a435b74af7cf048513b144cc3f4
William Correa <wilcorrea@gmail.com>
2023-04-20 10:01:01 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,14 +8,14 @@ const settings = {
       header: 'Development and Management of Software Projects',
       top: 'Resume',
       bio: 'I work with software development since 2007. I have a bachelor\'s degree in Computer Science and I try to keep in line with the market. I am always in contact with groups and communities of VueJS, PHP and other technologies throughout Brazil. As an evangelist in good practice, engineering and software architecture I am always looking to build quality applications and systems. I write articles and do talks with themes from git and devops to Javascript and VueJS.',
-      footer: 'Mini Bio',
+      footer: 'Mini Bio [PHP]',
       minibio: 'Working with PHP since 2009, has worked on projects in the educational and financial areas. Accustomed to getting involved with developer groups and communities, can commonly be seen in mentorships and help groups. Likes to contribute to open source and is always seeking knowledge and challenges.'
     },
     'pt_BR': {
       header: 'Desenvolvimento e Gestão de Projetos de Software',
       top: 'Resumo',
       bio: 'Trabalho com desenvolvimento de software desde 2007. Sou bacharel em Ciência da Computação e tento me manter alinhado com o mercado. Estou sempre em contato com grupos e comunidades de VueJS, PHP e outras tecnologias em todo o Brasil. Como evangelista em boas práticas, engenharia e arquitetura de software estou sempre em busca construir aplicações e sistemas de qualidade. Eu escrevo artigos e faço palestras com temas desde git e devops até Javascript e VueJS.',
-      footer: 'Mini Bio',
+      footer: 'Mini Bio [PHP]',
       minibio: 'Trabalhando com PHP desde 2009, já atuou em projetos nas áreas educacionais e financeiras. Acostumado a se envolver com grupos e comunidades de desenvolvedores pode ser visto comumente em mentorias e grupos de ajuda. Gosta de contribuir com open source e está sempre em busca de conhecimentos e desafios.'
     }
   },
